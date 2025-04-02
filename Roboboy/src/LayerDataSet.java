58f1436ac0c897dbd8aff4fe9843e7f112d681c1
CoderManDudeGuy <singha.shirshajit@gymstopp.org>
2025-04-02 15:17:27 +0200

    Version 0.2.1
    Sorry for forgetting about presentation day

---
+++ Roboboy/src/LayerDataSet.java
@@ -0,0 +1,9 @@
+public class LayerDataSet {
+    public int numberOfTilesX;
+    public  int numberOfTilesY;
+    public int tileWidth;
+    public int tileHeight;
+    public String [] tileSetData;
+    public String [] tilesData = null;
+
+}
