34f3413597bc49e957854ad2c36a25ff4963d42c
JustSomeoneXP <karpenko10102006@gmail.com>
2025-04-27 18:01:42 +0200

    Add files via upload

---
+++ Roboboy/out/production/Roboboy/Spieleumgebung Vorlage IJ/src/Boss.java
@@ -0,0 +1,305 @@
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Boss{
+    Rectangle enemyHitBox;
+    Ellipse2D viewRadius;
+    Ellipse2D attackRadius;
+    Point2D leftBottom;
+    Point2D leftTop;
+    Point2D rightBottom;
+    Point2D rightTop;
+    private Image image;
+    private int xPos;
+    private int yPos;
+    private int ySpeed = 0;
+    private final int width;
+    private final int height;
+    private Control control;
+    private int moveSeq = 0;
+    private int moveSeqSleep = 0;
+    private SpriteSheet sprites;
+    private ArrayList<Tile> neighbours;
+    private int lifePoints = 30;
+    boolean grounded;
+    int direction;
+    Random rng = new Random();
+    boolean active = false;
+    int wait =  0;
+    int movement = 2;
+    int jumpTop;
+    int jumpCounter;
+    boolean isJumping;
+    int attackCooldown = 0;
+    boolean isAttacking = false;
+    int walkingCooldown = 0;
+    int rngNumber;
+    boolean busy = false;
+
+
+    public Boss(int pXPos, int pYPos, int pWidth, int pHeight, SpriteSheet pSpriteSheet, Control pControl) {
+        sprites = pSpriteSheet;
+        image = sprites.getSpriteElement(0, 1);
+        xPos = pXPos;
+        yPos = pYPos;
+        width = pWidth;
+        height = pHeight;
+        control = pControl;
+        neighbours = Control.map.getLayerList().getFirst().getNeighbours(xPos+32, yPos+42, 1, 1);
+        leftBottom = new Point2D.Double(xPos+24, yPos+63);
+        leftTop = new Point2D.Double(xPos+24, yPos+21);
+        rightBottom = new Point2D.Double(xPos+40, yPos+63);
+        rightTop = new Point2D.Double(xPos+40, yPos+21);
+        enemyHitBox = new Rectangle(xPos+23, yPos+20, 18, 44);
+        viewRadius = new Ellipse2D.Double(xPos-240, yPos-240, 480, 480);
+        attackRadius = new Ellipse2D.Double(xPos-96, yPos-96, 192, 192);
+    }
+
+
+    public Point getLocation() {
+        return new Point(xPos, yPos);
+    }
+
+    public void setMove(Point pMove) {
+        int oldX = xPos;
+        xPos = (int) pMove.getX();
+        gravitation();
+        if (moveSeqSleep++ == 7) {
+            if (moveSeq < 2) {
+                moveSeq++;
+            } else {
+                moveSeq = 0;
+            }
+            moveSeqSleep = 0;
+        }
+        setCurrentImage((int) pMove.getX(), (int) pMove.getY(), moveSeq);
+
+        if (wallCheck()){
+            xPos=oldX;
+            movement=-movement;
+        }
+    }
+
+    public void gravitation(){
+        if (floorCheck()){
+            grounded = true;
+        } else {
+            ySpeed = 8;
+            yPos += ySpeed;
+        }
+    }
+
+    public void setCurrentImage(int pXMove, int pYMove, int pMoveSeq) {
+        if (pXMove == 1 && grounded) {
+            image = sprites.getSpriteElement(1, pMoveSeq);
+            direction=1;
+        } else if (pXMove == 1){
+            image = sprites.getSpriteElement(0, 2);
+            direction=1;
+        }
+
+        if (pXMove == -1 && grounded) {
+            image = sprites.getSpriteElement(2, pMoveSeq);
+            direction=2;
+        } else  if (pXMove == -1){
+            image = sprites.getSpriteElement(3, 0);
+            direction=2;
+        }
+
+        if (pXMove == 0 && grounded){
+            image = sprites.getSpriteElement(direction, 0);
+        } else if (pXMove == 0) {
+            if (direction == 1){
+                image = sprites.getSpriteElement(0, 2);
+            } else {
+                image = sprites.getSpriteElement(3, 0);
+            }
+        }
+    }
+
+    private boolean wallCheck(){
+        ArrayList<Layer> layerList = Control.map.getLayerList();
+        leftTop = new Point2D.Double(xPos+23, yPos+23);
+        Point2D leftMiddle = new Point2D.Double(xPos+23, yPos+42);
+        leftBottom = new Point2D.Double(xPos+23, yPos+63);
+        rightTop = new Point2D.Double(xPos+41, yPos+23);
+        Point2D rightMiddle = new Point2D.Double(xPos+41, yPos+42);
+        rightBottom = new Point2D.Double(xPos+41, yPos+63);
+        for (int i = 0; i < layerList.size(); i++) {
+            Tile temp = layerList.get(0).tiles[(int) leftTop.getX() / 64][(int) leftTop.getY() / 64];
+            Tile temp2 = layerList.get(0).tiles[(int) leftMiddle.getX() / 64][(int) leftMiddle.getY() / 64];
+            Tile temp3 = layerList.get(0).tiles[(int) leftBottom.getX() / 64][(int) leftBottom.getY() / 64];
+            Tile temp4 = layerList.get(0).tiles[(int) rightTop.getX() / 64][(int) rightTop.getY() / 64];
+            Tile temp5 = layerList.get(0).tiles[(int) rightMiddle.getX() / 64][(int) rightMiddle.getY() / 64];
+            Tile temp6 = layerList.get(0).tiles[(int) rightBottom.getX() / 64][(int) rightBottom.getY() / 64];
+            if (grounded){
+                if (temp.isBlocked() || temp2.isBlocked() || temp4.isBlocked() || temp5.isBlocked()) {
+                    return true;
+                }
+            } else {
+                if (temp.isBlocked() || temp2.isBlocked() || temp3.isBlocked() || temp4.isBlocked() || temp5.isBlocked() || temp6.isBlocked()) {
+                    return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
+    private boolean floorCheck(){
+        ArrayList<Layer> layerList = Control.map.getLayerList();
+        leftBottom = new Point2D.Double(xPos+26, yPos+64);
+        rightBottom = new Point2D.Double(xPos+37, yPos+64);
+        Tile temp = layerList.get(0).tiles[(int) leftBottom.getX() / 64][(int) leftBottom.getY() / 64];
+        Tile temp2 = layerList.get(0).tiles[(int) rightBottom.getX() / 64][(int) rightBottom.getY() / 64];
+        if(temp.isBlocked()||temp2.isBlocked()){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean ceilingCheck(){
+        ArrayList<Layer> layerList = Control.map.getLayerList();
+        leftTop = new Point2D.Double(xPos+26, yPos+20);
+        rightTop = new Point2D.Double(xPos+37, yPos+20);
+        Tile temp = layerList.get(0).tiles[(int) leftTop.getX() / 64][(int) leftTop.getY() / 64];
+        Tile temp2 = layerList.get(0).tiles[(int) rightTop.getX() / 64][(int) rightTop.getY() / 64];
+        if(temp.isBlocked()||temp2.isBlocked()){
+            return true;
+        }
+        return false;
+    }
+
+    public void paintMe(Graphics2D g2d) {
+        g2d.drawImage(image, xPos - Control.camera.getXOffset(), yPos - Control.camera.getYOffset(), width, height, null);
+        viewRadius = new Ellipse2D.Double(xPos - Control.camera.getXOffset()-208, yPos - Control.camera.getYOffset()-202, 480, 480);
+        g2d.setColor(Color.red);
+        g2d.fill(viewRadius);
+        attackRadius = new Ellipse2D.Double(xPos - Control.camera.getXOffset()-64, yPos - Control.camera.getYOffset()-61, 192, 192);
+        g2d.setColor(Color.black);
+        g2d.fill(attackRadius);
+        enemyHitBox = new Rectangle(xPos - Control.camera.getXOffset()+23, yPos - Control.camera.getYOffset()-36,  300, 100);
+        g2d.setColor(Color.blue);
+        g2d.fill(enemyHitBox);
+    }
+
+    public void jump(){
+        if (!ceilingCheck() && jumpTop < 1){
+            grounded=false;
+            if (jumpCounter < 40) {
+                if (jumpCounter == 39){
+                    movement=movement*2;
+                }
+                yPos -= 16;
+                jumpCounter++;
+                attackCooldown++;
+            } else if (jumpTop == 0) {
+                yPos += 2;
+                jumpTop++;
+                attackCooldown++;
+            } else {
+                isJumping=false;
+                isAttacking=false;
+                movement=movement/2;
+                jumpCounter = 0;
+                jumpTop=0;
+            }
+        } else if (jumpTop < 10) {
+            yPos += 2;
+            jumpTop++;
+        } else {
+            isJumping=false;
+            isAttacking=false;
+            jumpCounter = 0;
+            jumpTop=0;
+        }
+    }
+
+    public void damage(){
+        lifePoints-=10;
+    }
+
+    public void attack (Rectangle pPlayer){
+        if (wait==0) {
+            if (((attackRadius.intersects(pPlayer) && grounded) || isAttacking)&&!busy) {
+                isAttacking = true;
+                jump();
+//                System.out.println("JUMP");
+            } else if ((attackCooldown < 75&&attackCooldown!=0)&&!busy) {
+                attackCooldown++;
+                System.out.println(attackCooldown);
+            } else if (attackCooldown >= 75&&!busy) {
+                attackCooldown = 0;
+                wait++;
+            } else if (viewRadius.intersects(pPlayer)&&!busy) {
+                if (walkingCooldown<75) {
+                    if (enemyHitBox.getX() < pPlayer.getX() && !wallCheck()) {
+                        movement = 4;
+                    } else if (enemyHitBox.getX() > pPlayer.getX() && !wallCheck()) {
+                        movement = -4;
+                    }
+                    setMove(new Point(xPos + movement, yPos));
+                    walkingCooldown++;
+                } else if (walkingCooldown==75) {
+                        walkingCooldown=0;
+                        wait++;
+                }
+//                System.out.println("FOLLOW");
+            } else {
+                if (walkingCooldown==0){
+                    rngNumber = rng.nextInt(4);
+                    System.out.println(rngNumber);
+                }
+
+                if (rngNumber==3){
+                    busy = true;
+//                    System.out.println("dash");
+                    movement=movement*2;
+                    setMove(new Point(xPos + movement, yPos));
+                    movement=movement/2;
+                    walkingCooldown++;
+                    System.out.println(wallCheck());
+                    if (wallCheck()){
+                        System.out.println(wallCheck());
+                        wait++;
+                        walkingCooldown=0;
+                    }
+                } else {
+//                    System.out.println("ROAM");
+                    if (walkingCooldown<75) {
+                        setMove(new Point(xPos + movement, yPos));
+                        walkingCooldown++;
+                    } else if (walkingCooldown==75) {
+                        walkingCooldown=0;
+                        wait++;
+                    }
+                }
+//                setMove(new Point(xPos, yPos));
+
+            }
+
+        } else if (wait < 50) {
+            setMove(new Point(xPos, yPos));
+            wait++;
+        } else {
+            System.out.println("STOP WAIT");
+            setMove(new Point(xPos, yPos));
+            busy = false;
+            wait=0;
+        }
+
+//        if (attackRadius.intersects(pPlayer)) {
+//            //Jump on the position
+//            jump();
+//        } else if (viewRadius.intersects(pPlayer)){
+//            //dash slow, jump
+//        } else {
+//            //dash fast across arena, don't stop for player. When hitting wall, stun
+//            //walk
+//            //wait
+//        }
+    }
+}
---
+++ Roboboy/out/production/Roboboy/Spieleumgebung Vorlage IJ/src/KeyManager.java
@@ -0,0 +1,72 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyManager implements KeyListener {
+
+    private boolean[] keys;
+    public boolean up, down, left, right, action, jump, dash;
+
+    public boolean isTurning = false;
+    //public double degreeOfTankRotation = 0.0;
+    //public double degreeOfTowerRotation = 0.0;
+
+    public KeyManager(){
+        keys = new boolean[256];
+    }
+
+    public void update(){
+        up = keys[KeyEvent.VK_W];
+        down = keys[KeyEvent.VK_S];
+        left = keys[KeyEvent.VK_A];
+        right = keys[KeyEvent.VK_D];
+        action = keys[KeyEvent.VK_M];
+        jump = keys[KeyEvent.VK_SPACE];
+        dash = keys[KeyEvent.VK_SHIFT];
+    }
+
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+        }
+
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {;
+        }
+    }
+
+    public void releaseAll(){
+        keys[KeyEvent.VK_W] = false;
+        keys[KeyEvent.VK_SPACE] = false;
+        keys[KeyEvent.VK_A] = false;
+        keys[KeyEvent.VK_D] = false;
+        keys[KeyEvent.VK_E] = false;
+        keys[KeyEvent.VK_M] = false;
+        update();
+    }
+
+
+    public void keyTyped(KeyEvent e) {
+    }
+}
