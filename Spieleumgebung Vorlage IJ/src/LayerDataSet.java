5430b5dbf287249443b7e0e19b138647f51327e7
CoderManDudeGuy <singha.shirshajit@gymstopp.org>
2025-05-22 19:58:04 +0200

    Delete Spieleumgebung Vorlage IJ directory

--- Spieleumgebung Vorlage IJ/src/LayerDataSet.java
+++
@@ -1,9 +0,0 @@
-public class LayerDataSet {
-    public int numberOfTilesX;
-    public  int numberOfTilesY;
-    public int tileWidth;
-    public int tileHeight;
-    public String [] tileSetData;
-    public String [] tilesData = null;
-
-}
